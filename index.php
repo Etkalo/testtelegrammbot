ee125f7fe4ca6976bb0e5326462f1eedd3cf65d7
SimonE <semen.etkalo@gmail.com>
2017-10-17 01:45:58 +0300

    telegramm api

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-var_dump(scandir('api'));die;
+var_dump(glob('*.php'));die;
 
 include 'api/Api.php';
 
